89128dd2ab5afd52bde15cf6c7eb7368b2ff86b2
Anus125 <108853908+Anus125@users.noreply.github.com>
2025-10-21 17:56:36 +0500

    editing

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ const navMenu = document.getElementById("nav-menu");
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+const toTopBtn = document.getElementById("toTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 100) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+});
+
+toTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
